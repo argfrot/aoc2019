8495a25290d3efd72539da25c94b1bec6eefe2c8
Graham Williamson <526612+argfrot@users.noreply.github.com>
2019-12-25 11:42:17 +0000

    Day 12, part 2.

--- day12/run.py
+++ day12/run.py
@@ -5,6 +5,9 @@ import operator
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Callable, Any, List
+from itertools import chain, combinations, product
+from functools import reduce
+from math import gcd
 
 # <x=5, y=-1, z=5>
 COORD = '([A-Za-z]*)=(-?[0-9]*)'
@@ -65,6 +68,26 @@ class Moon:
             elif pos < other_pos:
                 self.velocity[dimension] += 1
 
+    def apply_gravity_2ways(self, other):
+        for dimension, (pos, other_pos) in enumerate(zip(self.position, other.position)):
+            if pos > other_pos:
+                self.velocity[dimension] -= 1
+                other.velocity[dimension] += 1
+            elif pos < other_pos:
+                self.velocity[dimension] += 1
+                other.velocity[dimension] -= 1
+
+    def apply_gravity_2ways_1d(self, other, dimension):
+        if self.position[dimension] > other.position[dimension]:
+            self.velocity[dimension] -= 1
+            other.velocity[dimension] += 1
+        elif self.position[dimension] < other.position[dimension]:
+            self.velocity[dimension] += 1
+            other.velocity[dimension] -= 1
+
+    def move_1d(self, dimension):
+        self.position[dimension] = self.position[dimension] + self.velocity[dimension]
+
     def move(self):
         self.position = list(map(operator.add, self.position, self.velocity))
     
@@ -73,6 +96,12 @@ class Moon:
 
     def kinetic_energy(self):
         return sum(map(abs, self.velocity))
+
+    def state_1d(self, dimension):
+        return (self.position[dimension], self.velocity[dimension])
+
+    def state(self):
+        return tuple(chain(self.position, self.velocity))
 
 def part1(steps=10, input=get_input, display=False):
     moons = [Moon([v['x'], v['y'], v['z']]) for v in input()]
@@ -89,7 +118,57 @@ def part1(steps=10, input=get_input, display=False):
         step += 1
     return sum((moon.potential_energy() * moon.kinetic_energy() for moon in moons))
 
+# too slow!
+def part2(steps=10, input=get_input, display=False):
+    moons = [Moon([v['x'], v['y'], v['z']]) for v in input()]
+    step = 1
+    seen_states = set()
+    while step <= steps:
+        current_state = tuple(moon.state() for moon in moons)
+        if current_state in seen_states:
+            return (step-1, current_state)
+        seen_states.add(current_state)
+        if display or step%100000==0: print(f'Step={step} states={len(seen_states)}')
+        for moon, other_moon in combinations(moons, 2):
+            moon.apply_gravity_2ways(other_moon)
+        for moon in moons:
+            moon.move()
+            if display: print(f'moon={moon} pot={moon.potential_energy()} kin={moon.kinetic_energy()}')
+        step += 1
+    return None
+
+# simulate per dimension: the sequence repeats independently
+def part2_2(input=get_input):
+    periods = [get_period_for_dimension(dim, input) for dim in range(3)]
+    # prod = reduce(operator.mul, periods, 1)
+    # gcd_ = reduce(gcd, periods, periods[0])
+    lcm = reduce(lambda a,b: (a*b)//(gcd(a,b)), periods)
+    return lcm
+
+def get_period_for_dimension(dimension, input=get_input):
+    moons = [Moon([v['x'], v['y'], v['z']]) for v in input()]
+    step = 1
+    seen_states = dict()
+    state_list = []
+    while True:
+        current_state = tuple(moon.state_1d(dimension) for moon in moons)
+        state_list.append(current_state)
+        if current_state in seen_states:
+            return step-1
+        seen_states[current_state] = step-1
+        for moon, other_moon in combinations(moons, 2):
+            moon.apply_gravity_2ways_1d(other_moon, dimension)
+        for moon in moons:
+            moon.move_1d(dimension)
+        step += 1
+    return None
+
+
 if __name__ == '__main__':
     print(part1(steps=10, input=test_input)) # 179
     print(part1(steps=100, input=test_input2)) # 1940
     print(part1(steps=1000)) # 7928
+    print(part2(steps=3000, input=test_input)) # 2772
+    # print(part2(steps=5000000000, input=test_input2)) # should be 4686774924 but runs forevvvvvver
+    print(part2_2(input=test_input2)) # 4686774924
+    print(part2_2()) # 518311327635164
